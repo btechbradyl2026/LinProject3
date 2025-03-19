329426e6cbff6f4797183c6540f72b43e5b4afaa
BT_1N3_01 <sample@bths.edi>
2025-03-19 13:58:26 -0400

    Created new helper methods and initializers

--- src/PetStore.java
+++ src/PetStore.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PetStore {
@@ -11,26 +12,58 @@ public class PetStore {
     int turns;
 
 
-    public void generatePet(int pets, String cd) {
+    public void generatePet(String cd) {
         Pet currentPet;
-        for (int i = 0; i < pets; i ++) {
+        Pet[] newRow = new Pet[5];
+        for (int i = 0; i < 5; i ++) {
             if (cd.equals("cat")) {
-
+                int price = priceGen();
+                double want = ((double) price/100 - 100) * -1;
+                currentPet = new Cat(nameGen(), ageGen(), false ,genderGen(), price, want);
             } else if (cd.equals("dog")) {
-                currentPet = new Dog(nameGen(), ageGen(), )
+                int price = priceGen();
+                double want = ((double) price/100 - 100) * -1;
+                currentPet = new Dog(nameGen(), ageGen(), false ,genderGen(), price, want);
             } else {
                 System.out.println("Please enter \"cat\" or \"dog\" in the second slot");
                 break;
             }
+            newRow[i] = currentPet;
         }
+        Pet[][] temp = new Pet[pets.length + 1][5];
+        temp = pets.clone();
+        temp[temp.length - 1] = newRow;
+        pets = temp;
+        System.out.println(Arrays.deepToString(pets));
     }
 
     public String nameGen() {
         return names.get(1 + (int)(Math.random() * ((1177) + 1)));
     }
 
+    public String genderGen() {
+        if (1 + (int)(Math.random() * ((1) + 1)) == 2) {
+            return "male";
+        } else {
+            return "female";
+        }
+    }
+
+    public int priceGen() {
+        return 1 + (int)(Math.random() * ((99) + 1));
+    }
+
     public int ageGen() {
         return 1 + (int)(Math.random() * ((14) + 1));
+    }
+
+    public void printPets() {
+        for (Pet[] pet : pets) {
+            for (int j = 0; j < pets[0].length; j++) {
+                System.out.print(pet[j].getName());
+            }
+            System.out.println();
+        }
     }
 
 
